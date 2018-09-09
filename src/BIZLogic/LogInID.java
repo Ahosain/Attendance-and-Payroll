2f7b4f8f5c993bdba79d30ab4ed19da6598eee2a
Fahid <fahidahmed16@gmail.com>
2018-09-09 12:51:15 +0600

    Message Combolsarry

---
+++ src/BIZLogic/LogInID.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BIZLogic;
+
+/**
+ *
+ * @author PC_65
+ */
+public class LogInID {
+    public String teacherId;
+    public LogInID(String teacherID) {
+        System.out.println(teacherID);
+        this.teacherId= teacherID;
+    }
+}
---
+++ src/BIZLogic/LoginData.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BIZLogic;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Anwar Hosain
+ */
+public class LoginData {
+    private String userid;
+
+    public LoginData() {
+    }
+
+    public LoginData(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+   
+    
+    
+}
